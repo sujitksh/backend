b9b29a3c589aeb9b061a3557363cda364abb95ea
skg <sujitkushwaha@gmail.com>
2023-09-14 17:13:11 +0530

    changes in index file

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const port = 8000
 app.get("/test",(req,res)=>{
     return res.send("Hello World")
 })
-app.get("/get",(req,res)=>{
-    return res.send("This is my first page")
-})
-app.get("/create",(req,res)=>{
-    return res.send("I created my first blog which is very interesting")
-})
+
 app.listen(port,()=>{
     console.log(`running port ${port}`)
 })
